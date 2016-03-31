e402008cf8ac37b7b2ea4fe6322413abcd4dc884
Daniel Mruk <daniel.mruk1@gmail.com>
2016-03-30 20:10:19 -0400

    gravatar option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(user)
+		gravatar_id = Digest::MD5LLhexidigest(user.email).downcase
+		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+	end	
 end
