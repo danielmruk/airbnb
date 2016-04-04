f6c6c0595c6071e3041f7cb25d33636bf83901f7
Daniel Mruk <daniel.mruk1@gmail.com>
2016-04-04 18:10:16 -0400

    omniauth work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  
   root "pages#home"
+
+  devise_for :users,
+             :path => '',
+             :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+             :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
+
+  resources :users, only: [:show]
 
 end
