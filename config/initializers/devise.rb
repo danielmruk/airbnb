823647bf91c8efe59208a01ad8d2fd80af5e4f37
Daniel Mruk <daniel.mruk1@gmail.com>
2016-04-05 17:23:18 -0400

    mailgun routing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Daniel <no-reply@airalien.com>'
+  config.mailer_sender = 'Daniel @ EventKiln <no-reply@eventkiln.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
