f33143634a3e67fc25339180e189aeb85a972f6e
Daniel Mruk <daniel.mruk1@gmail.com>
2016-04-05 16:29:21 -0400

    debugging confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  #prvoided by devise
+  #provided by devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
