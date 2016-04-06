4f662a5b18a7aa437af81748a3516352a2b89bd0
Daniel Mruk <daniel.mruk1@gmail.com>
2016-04-05 23:20:56 -0400

    mailgun heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://intense-plains-33356.heroku.com',
+  :domain         => 'yourapp.heroku.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
