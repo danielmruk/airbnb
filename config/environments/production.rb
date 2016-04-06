80d06e1610a6c32f98fc18f706a56ea0920d250f
Daniel Mruk <daniel.mruk1@gmail.com>
2016-04-05 23:04:38 -0400

    routing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'intense-plains-33356.heroku.com',
+  :domain         => 'https://intense-plains-33356.heroku.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
